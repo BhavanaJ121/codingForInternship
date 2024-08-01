b774bc03baa3d2a4d2aa6d3861339c0837fa6ff9
Bhavana <bhavanajayanth313@gmail.com>
2024-08-01 11:31:31 -0400

    serpapi wrapped in class

--- routers/serapi_project/serpapi.py
+++ routers/serapi_project/serpapi.py
@@ -14,37 +14,49 @@ router = APIRouter(
 client = serpapi.Client(api_key=api_key)
 
 
+class SerpapiSearch:
+    def __init__(self, query):
+        self.query = query
+        self.result = None
+
+    def get_result(self, text_or_image):
+        engine_type = ""
+        if text_or_image == "text":
+            engine_type = "google"
+        elif text_or_image == "image":
+            engine_type = "google_images"
+        result = client.search(
+            q=self.query,
+            engine=engine_type,
+            hl='en',
+            gl='us'
+        )
+        self.result = result
+
+    def format_result(self, text_or_image):
+        formatted_result = {}
+        if text_or_image == "text":
+            for item in self.result['organic_results']:
+                formatted_result[item['title']] = ["Link: " + item['link'], "Snippet: " + item['snippet']]
+        elif text_or_image == 'image':
+            for item in self.result['images_results']:
+                formatted_result[item['title']] = ["Link: " + item['link'], "Image: " + item['original']]
+        return formatted_result
+
+
 @router.get("/")
 async def search(query: str):
-    result = client.search(
-        q=query,
-        engine='google',
-        hl='en',
-        gl='us'
-    )
-    formatted_result = {}
-    for item in result['organic_results']:
-        '''formatted_result += item['title']
-        formatted_result += item['link']
-        formatted_result += item['snippet']
-        formatted_result += "-------------" '''
-        formatted_result[item['title']] = ["Link: " + item['link'], "Snippet: " + item['snippet']]
-
+    search_object = SerpapiSearch(query)
+    search_object.get_result("text")
+    formatted_result = search_object.format_result("text")
     return {"result": formatted_result}
 
 
 @router.get("/image")
 async def search_image(query: str):
-    result = client.search(
-        q=query,
-        engine='google_images',
-        hl='en',
-        gl='us'
-    )
-    formatted_result = {}
-    for item in result['images_results']:
-        formatted_result[item['title']] = ["Link: " + item['link'], "Image: " + item['original']]
-
+    search_image_object = SerpapiSearch(query)
+    search_image_object.get_result("image")
+    formatted_result = search_image_object.format_result("image")
     return {"result": formatted_result}
 
 
