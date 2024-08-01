5eca901bba60e7c9c009c7f8872b8efcaf7bf52e
Bhavana <bhavanajayanth313@gmail.com>
2024-08-01 11:16:03 -0400

    pdf api wrapped in class

--- routers/pdf_project/pdf_main.py
+++ routers/pdf_project/pdf_main.py
@@ -13,6 +13,30 @@ router = APIRouter(
 )
 
 
+class PdfReader:
+    def __init__(self, filename):
+        self. filename = filename
+        self.text = ""
+
+    def read_pdf(self):
+        file = open(self.filename, 'rb')
+        reader = PyPDF2.PdfReader(file)
+        text = ""
+        for i in range(len(reader.pages)):
+            text += str(reader.pages[i].extract_text())
+        file.close()
+        self.text = text
+        return text
+
+    def create_model(self, db):
+        pdfs_model = model.Pdfs()
+        pdfs_model.name = str(self.filename)
+        pdfs_model.content = self.text
+
+        db.add(pdfs_model)
+        db.commit()
+
+
 def get_db():
     try:
         db = SessionLocal()
@@ -22,26 +46,14 @@ def get_db():
 
 
 @router.post('/upload-file')
-async def read_pdf(uploaded_file: UploadFile = File(...), db: Session = Depends(get_db)):
+async def read_pdf_api(uploaded_file: UploadFile = File(...), db: Session = Depends(get_db)):
     file_content = await uploaded_file.read()
     with open(uploaded_file.filename, "wb") as f:
         f.write(file_content)
 
-    file = open(uploaded_file.filename, 'rb')
-    reader = PyPDF2.PdfReader(file)
-    text = ""
-    for i in range(len(reader.pages)):
-        text += str(reader.pages[i].extract_text())
-    file.close()
-
-    # text = read_pdf(uploaded_file.filename)
-
-    pdfs_model = model.Pdfs()
-    pdfs_model.name = str(uploaded_file.filename)
-    pdfs_model.content = text
-
-    db.add(pdfs_model)
-    db.commit()
+    pdf_object = PdfReader(uploaded_file.filename)
+    text = pdf_object.read_pdf()
+    pdf_object.create_model(db)
 
     return {"content": text}
 
